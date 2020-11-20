7cf75925af5ddd66ed6f7b383c12fbde7d98b6bf
1813381673 <1813381673@qq.com>
2020-11-20 18:16:01 +0800

    202.11.10打卡

---
+++ src/middle/对链表进行插入排序.java
@@ -0,0 +1,57 @@
+package middle;
+
+
+/**
+ * @author xsj
+ * @Date 2020/11/20 9:49
+ *
+ * No.147
+ *
+ * 插入排序算法：
+ *
+ * 插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+ * 每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+ * 重复直到所有输入数据插入完为止。
+ *  
+ *示例 1：
+ *
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ *
+ * 示例 2：
+ *
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/insertion-sort-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class 对链表进行插入排序 {
+
+    public static ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode lastSorted = head, curr = head.next;
+        while (curr != null) {
+            if (lastSorted.val <= curr.val) {
+                lastSorted = lastSorted.next;
+            } else {
+                ListNode prev = dummyHead;
+                while (prev.next.val <= curr.val) {
+                    prev = prev.next;
+                }
+                lastSorted.next = curr.next;
+                curr.next = prev.next;
+                prev.next = curr;
+            }
+            curr = lastSorted.next;
+        }
+        return dummyHead.next;
+    }
+
+}
+
