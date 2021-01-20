e06041494c8fe474ab78e7dff0a0f5a99464d498
1813381673 <1813381673@qq.com>
2021-01-20 11:44:09 +0800

    2021.01.20打卡，只配做简单题

---
+++ src/simple/三个数的最大乘积.java
@@ -0,0 +1,70 @@
+package simple;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author TT
+ * @Date 2021/1/20 11:34
+ * No.628
+ * 给定一个整型数组，在数组中找出由三个数组成的最大乘积，并输出这个乘积。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3]
+ * 输出: 6
+ * 示例 2:
+ *
+ * 输入: [1,2,3,4]
+ * 输出: 24
+ * 注意:
+ *
+ * 给定的整型数组长度范围是[3,10^4]，数组中所有的元素范围是[-1000, 1000]。
+ * 输入的数组中任意三个数的乘积不会超出32位有符号整数的范围。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-product-of-three-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ *
+ */
+public class 三个数的最大乘积 {
+
+    // 时间复杂度nlogn  空间复杂度 n
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        return Math.max(nums[0] * nums[1] * nums[len - 1], nums[len - 1] * nums[len - 2] * nums[len - 3]);
+    }
+    // 时间复杂度 n  空间复杂度1
+    public int maximumProduct2(int[] nums) {
+        // 最小的和第二小的
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        // 最大的、第二大的和第三大的
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+
+        for (int x : nums) {
+            if (x < min1) {
+                min2 = min1;
+                min1 = x;
+            } else if (x < min2) {
+                min2 = x;
+            }
+            if (x > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = x;
+            } else if (x > max2) {
+                max3 = max2;
+                max2 = x;
+            } else if (x > max3) {
+                max3 = x;
+            }
+        }
+        return Math.max(min1 * min2 * max1, max1 * max2 * max3);
+    }
+
+
+}
