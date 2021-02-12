2d89086186f8dfd626fc47bbfac31f4eadef4257
1813381673 <1813381673@qq.com>
2021-02-12 08:37:26 +0800

    2021.02.12打卡,根据递推公式，暴力破解

---
+++ src/simple/杨辉三角II.java
@@ -0,0 +1,46 @@
+package simple;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author TT
+ * @Date 2021/2/12 8:31
+ * No.119
+ * 给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行。
+ *
+ *
+ *
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ *
+ * 示例:
+ *
+ * 输入: 3
+ * 输出: [1,3,3,1]
+ * 进阶：
+ *
+ * 你可以优化你的算法到 O(k) 空间复杂度吗？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/pascals-triangle-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class 杨辉三角II {
+
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> ans = new ArrayList();
+        for (int i = 0; i <= rowIndex; ++i) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j));
+                }
+            }
+            ans.add(row);
+        }
+        return ans.get(rowIndex);
+    }
+}
