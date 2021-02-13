02b1f0302ffe07655352f8aff2a7e0e9c5acdceb
1813381673 <1813381673@qq.com>
2021-02-13 21:41:18 +0800

    2021.02.13打卡,找技巧解题

---
+++ src/simple/找到所有数组中消失的数字.java
@@ -0,0 +1,46 @@
+package simple;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author TT
+ * @Date 2021/2/13 9:44
+ * No.448
+ * 给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+ *
+ * 找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+ *
+ * 您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+ *
+ * 示例:
+ *
+ * 输入:
+ * [4,3,2,7,8,2,3,1]
+ *
+ * 输出:
+ * [5,6]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class 找到所有数组中消失的数字 {
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n = nums.length;
+        for (int num : nums) {
+            int x = (num - 1) % n;
+            nums[x] += n;
+        }
+        List<Integer> ans = new ArrayList();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= n) {
+                ans.add(i + 1);
+            }
+        }
+        return ans;
+
+    }
+}
