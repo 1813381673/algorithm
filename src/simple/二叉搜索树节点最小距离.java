76e129c74833f134453b4b8c0d44a67bdfe8fca6
1813381673 <1813381673@qq.com>
2021-04-13 08:35:38 +0800

    2021.04.13打卡，二叉搜索树

---
+++ src/simple/二叉搜索树节点最小距离.java
@@ -0,0 +1,73 @@
+package simple;
+
+
+/**
+ * @author TT
+ * @Date 2021/4/13 8:31
+ * No.783
+ * 给你一个二叉搜索树的根节点 root ，返回 树中任意两不同节点值之间的最小差值 。
+ *
+ * 注意：本题与 530：https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst/ 相同
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [4,2,6,1,3]
+ * 输出：1
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,0,48,null,null,12,49]
+ * 输出：1
+ *  
+ *
+ * 提示：
+ *
+ * 树中节点数目在范围 [2, 100] 内
+ * 0 <= Node.val <= 105
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-distance-between-bst-nodes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class 二叉搜索树节点最小距离 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        int pre;
+        int ans;
+        public int minDiffInBST(TreeNode root) {
+            ans = Integer.MAX_VALUE;
+            pre = -1;
+            dfs(root);
+            return ans;
+        }
+
+        public void dfs(TreeNode root) {
+            if (root == null) return;
+            dfs(root.left);
+            if (pre == -1) {
+                pre = root.val;
+            } else {
+                ans = Math.min(ans, root.val - pre);
+                pre = root.val;
+            }
+            dfs(root.right);
+        }
+    }
+}
